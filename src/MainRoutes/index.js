9fd1588aa2d5ce3aec2ebea16ab8efb61842c05f
Guilherme Carvalho <guiazca@gmail.com>
2022-02-18 12:29:37 +0000

    Created MainRoutes

---
+++ src/MainRoutes/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    Routes,
+    Route
+} from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Detail from '../pages/Detail';
+
+export default function MainRoutes(){
+    return (
+        <Routes>
+            <Route path='/' element={<Dashboard />}/>
+            <Route path='/Detail' element={<Detail />}/>
+        </Routes>
+    )
+}
