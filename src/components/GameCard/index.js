4aaddc1944b6c5cfdb7dddf38c62a3ab43253bf9
Guilherme Carvalho <guiazca@gmail.com>
2022-02-18 13:20:56 +0000

    Clean unsed Code

--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box, Image, Badge, Flex} from '@chakra-ui/react'
+import { Box, Image, Badge } from '@chakra-ui/react'
 import { StarIcon } from '@chakra-ui/icons'
 import './gameCard.css'
 
