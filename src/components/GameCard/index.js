6b3100442e2a4587163b0ba889aeccdeb58061f2
Guilherme Carvalho <guiazca@gmail.com>
2022-02-18 12:28:47 +0000

    Create Component Gamecard

---
+++ src/components/GameCard/index.js
@@ -0,0 +1,66 @@
+import React from "react"
+import { Box, Image, Badge, Flex} from '@chakra-ui/react'
+import { StarIcon } from '@chakra-ui/icons'
+import './gameCard.css'
+
+import { games } from '../../constants/games'
+
+export default function GameCard() {
+   return (
+       <>
+       {games.map((_,i) => {
+           console.log(games[i])
+           return (
+            <Box className="box" maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' marginBottom="30px" height="100px">
+            <Image src={games[i].background_image} alt={`${games[i].name_original} game`} />
+      
+            <Box p='6'>
+              <Box display='flex' alignItems='baseline'>
+                <Badge borderRadius='full' px='2' colorScheme='teal'>
+                  {games[i].esrb_rating?.name}    
+                </Badge>
+                <Box
+                color='gray.500'
+                fontWeight='semibold'
+                letterSpacing='wide'
+                fontSize='xs'
+                textTransform='uppercase'
+                ml='2'
+              >
+                  {games[i].genres.map((_,genresIndex) => {
+                      return `${games[i].genres[genresIndex].name} `
+                  })}
+                   </Box>
+              </Box>
+      
+              <Box
+                mt='1'
+                fontWeight='semibold'
+                as='h4'
+                lineHeight='tight'
+                isTruncated
+              >
+                {games[i].name_original}
+              </Box>
+      
+             <Box display='flex' mt='2' alignItems='center'>
+                {Array(5)
+                  .fill('')
+                  .map((_,starIndex) => (
+                    <StarIcon
+                      key={starIndex}
+                      color={starIndex < games[i].rating ? 'teal.500' : 'gray.300'}
+                    />
+                  ))}
+              </Box>
+            </Box>
+          </Box>
+           )
+            
+       })}
+       
+      </>
+    )
+}
+
+
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HeaderDiv } from './styles'
+
+export default function Header(){
+    return (
+       <HeaderDiv>
+           <h1>EUINTENDO</h1>
+       </HeaderDiv>
+    )
+}
