9a9204ade3aeb344e8917d5af98599349c761f92
Guilherme Carvalho <guiazca@gmail.com>
2022-02-22 15:36:20 +0000

    added Theme Added chakra dark theme

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import {BrowserRouter as Router } from 'react-router-dom'
 import MainRoutes from './MainRoutes'
 import { ChakraProvider } from '@chakra-ui/react'
 import GlobalFonts from '../src/assets/fonts/fonts';
+import theme from "./theme"
 
 function App() {
   return (
     <Router>
       <GlobalFonts />
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <MainRoutes />
       </ChakraProvider>      
     </Router>    
