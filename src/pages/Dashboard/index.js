0c42c1aae3f9c22e7ba11d9ff097d46da12f0a9a
Guilherme Carvalho <guiazca@gmail.com>
2022-02-22 10:33:26 +0000

    adjust

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -10,7 +10,6 @@ export default function Dashboard() {
         <>
         <Header />
         <Container className="containerDasboard">
-           
             <GameCard />
         </Container>
         </>
