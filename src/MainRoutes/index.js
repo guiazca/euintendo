24c4057f4e2aa8093954cf5d4fc124e04b155833
Guilherme Carvalho <guiazca@gmail.com>
2022-03-04 17:51:50 +0000

    add new pages to MainRoutes

--- src/MainRoutes/index.js
+++ src/MainRoutes/index.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import {
     Routes,
-    Route
+    Route,
 } from 'react-router-dom';
+
 
 import Dashboard from '../pages/Dashboard';
 import Detail from '../pages/Detail';
+import Login from '../pages/Login';
+import Register from '../pages/Register'
+import ResetPassword from "../pages/ResetPassword"
 
 export default function MainRoutes(){
+
+
     return (
         <Routes>
-            <Route path='/' element={<Dashboard />}/>
-            <Route path='/Detail/:id' element={<Detail />}/>
+            <Route path='/Register' element={<Register />}/>
+            <Route path='/ResetPassword' element={<ResetPassword />}/>
+            <Route path='/' element={<Login />}/>            
+            <Route path='/Dashboard' element={<Dashboard />}/>
+            <Route path='/Detail/:id' element={<Detail />}/>          
         </Routes>
     )
 }
